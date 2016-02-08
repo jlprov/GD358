e2e6b558147a3fa2ed07597b4e05244d52e6c429
Agnes <axk55@psu.edu>
2016-02-08 16:56:54 -0500

    Fixed some typos in tables that would not compile. Also edited msnew.tex so that now it does compile.

---
+++ githubcommands.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+git add --all
+git commit
+git push origin master
+
+#list updates about local directory
+#git branch -v
+
+#list remote directories attached to local directory
+#git remote -v
