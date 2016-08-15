c6888a60b08dd7f866e58375fed5d38df7ce72f7
jlprov <jlp@aries.physics.udel.edu>
2016-08-15 11:56:39 -0400

    uploading

---
+++ tmp1/githubcommands.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+#series of commands to push changes made locally to server
+git add --all
+git commit
+git push origin master
+
+#series of commands to download changes made on server
+#git fetch upstream
+#git checkout master
+#git merge upstream/master
+
+#list updates about local directory
+#git branch -v
+
+#list remote directories attached to local directory
+#git remote -v
